914bf33d60c3449341fc6b63d367d34529094c42
Tyvation <daniel300068@gmail.com>
2025-07-30 01:08:36 +0800

    s

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,17 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig: NextConfig = {
-  output: 'export',
+  output: isProd ? 'export' : undefined,
   trailingSlash: true,
   images: {
     unoptimized: true,
   },
+  reactStrictMode: true,
+  assetPrefix: isProd
+    ? 'https://tyvation.github.io/'
+    : undefined
 };
 
 export default nextConfig;
